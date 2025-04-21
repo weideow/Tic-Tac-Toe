0b5ea2def4b0dbc9ec365e87d51eecf36ff50652
weideow <owweide@hotmail.com>
2025-04-21 16:20:40 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {useState} from 'react';
+
+
+function Square() {
+  const[value, setValue] = useState(null);
+
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button 
+    className="square"
+    onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+
+export default function Board() {
+
+  return (
+
+    <>
+    <div className="board-row">
+    <Square />
+    <Square />
+    <Square />
+    </div>
+    <div className="board-row">
+    <Square />
+    <Square />
+    <Square />
+    </div>
+    <div className="board-row">
+    <Square />
+    <Square />
+    <Square />
+    </div>
+    </>
+  );
+}
+
